91caa43a389c90a1db7bef4400c310f86562000e
kostas-kavala <kostas@manbiz.gr>
2016-01-09 10:45:23 +0200

    Create add-client-form.php

---
+++ add-client-form.php
@@ -0,0 +1,23 @@
+<form id="add-client" method="post" action="add-client.php">
+  First Name
+  <input type="text" name="firstname">
+  Last Name
+  <input type="text" name="lastname">
+  Company Name
+  <input type="text" name="companyname">
+  Email
+  <input type="text" name="email">
+  Address
+  <input type="text" name="address1">
+  City
+  <input type="text" name="city">
+  State
+  <input type="text" name="state">
+  Post Code
+  <input type="text" name="postcode">
+  Country
+  <input type="text" name="country">
+  Phone
+  <input type="text" name="phonenumber">
+  <input type="submit" id="submit" value="Add client" />
+</form>
